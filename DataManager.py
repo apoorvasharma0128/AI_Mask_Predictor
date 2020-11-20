7f109623a06856868bba59bed6507fe090d15f84
apoorvasharma0128 <apoorvasharma0128@gmail.com>
2020-11-20 18:45:15 -0500

    Model Evaluation

--- DataManager.py
+++ DataManager.py
@@ -1,7 +1,3 @@
-#credit :https://www.kaggle.com/andrewmvd/face-mask-detection
-#credit : https://www.kaggle.com/alessiocorrado99/animals10
-#credit :https://www.kaggle.com/ashwingupta3012/human-faces  ----for without mask :needs to be dowloaded
-
 from PIL import Image, ImageOps
 import os
 import shutil
@@ -18,48 +14,46 @@ IMG_SIZE = (128,128)
 train_folder = os.getcwd()+"/Database/training-data"
 image_database=os.getcwd()+"/Database"
 
-class DataManager:
-    def resizeImage(src_image, size, bg_color="white"):
-        try:
-            src_image.thumbnail(size, Image.ANTIALIAS)
-            new_image = Image.new(RGB, size, bg_color)
-            new_image.paste(src_image, (int((size[0] - src_image.size[0]) / 2), int((size[1] - src_image.size[1]) / 2)))
-            return new_image
-        except:
-            print("Issue occured while resizing images")
+def resizeImage(src_image, size, bg_color="white"):
+    try:
+        src_image.thumbnail(size, Image.ANTIALIAS)
+        new_image = Image.new(RGB, size, bg_color)
+        new_image.paste(src_image, (int((size[0] - src_image.size[0]) / 2), int((size[1] - src_image.size[1]) / 2)))
+        return new_image
+    except:
+        print("Issue occured while resizing images")
 
-    def transformImages(self):
-        try:
-            print(os.path.exists(train_folder))
-            if os.path.exists(train_folder):
-                shutil.rmtree(train_folder)
-            for root, folders, files in os.walk(image_database):
-                print("root"+root)
-                print("Classes :",folders)
-                for sub in folders:
-                    print('processing folder ' + sub)
-                    newLoc = os.path.join(train_folder,sub)
-                    if not os.path.exists(newLoc):
-                        os.makedirs(newLoc)
-                    file_names = os.listdir(os.path.join(root,sub))
-                    for file in file_names:
-                        if file!=DS_STORE:
-                            resized_image = self.resizeImage(Image.open(os.path.join(root,sub,file)),IMG_SIZE)
-                            newPath = os.path.join(newLoc, file)
-                            resized_image.save(newPath)
+def transformImages():
+    msg=""
+    try:
+        train_folder=os.getcwd()+"/Database/training-data"
+        if os.path.exists(train_folder):
+            shutil.rmtree(train_folder)
+        for root, folders, files in os.walk(image_database):
+            print("Classes :",folders)
+            for sub in folders:
+                print('processing folder ' + sub)
+                newLoc = os.path.join(train_folder,sub)
+                if not os.path.exists(newLoc):
+                    os.makedirs(newLoc)
+                file_names = os.listdir(os.path.join(root,sub))
+                for file in file_names:
+                    if file!=DS_STORE:
+                        resized_image = resizeImage(Image.open(os.path.join(root,sub,file)),IMG_SIZE)
+                        newPath = os.path.join(newLoc, file)
+                        resized_image.save(newPath)
 
-            print("Transformation Step complete")
-        except:
-            print("Error Occured in TransformData")
+        mgs=("Transformation Step complete")
+    except:
+        mgs=("Error Occured in TransformData")
+    return msg
 
-
-    def loadImages(data_path):
-        transformation = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
-        dataset = torchvision.datasets.ImageFolder(root=data_path,transform=transformation)
-        train_size = int(0.6 * len(dataset))
-        test_size = len(dataset) - train_size
-        # use torch.utils.data.random_split for training/test split
-        train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-        train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=50,num_workers=2,shuffle=False)
-        test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=50,num_workers=2,shuffle=False)
-        return train_loader, test_loader
+def loadImages(data_path):
+    transformation = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+    dataset = torchvision.datasets.ImageFolder(root=data_path,transform=transformation)
+    train_size = int(0.6 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+    train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=50,num_workers=0,shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=50,num_workers=0,shuffle=False)
+    return train_loader, test_loader
--- Model.py
+++ Model.py
@@ -1,102 +1,123 @@
-from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
-import torchvision.transforms as transforms
-import torchvision.datasets
-from DataManager import DataManager as dm
+import torch.nn.functional as F
+import DataManager as dm
 import os
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import numpy as np
+
 num_epochs = 4
-num_classes = 2
+num_classes = 3
 learning_rate = 0.001
-# transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-# train_loader = torch.utils.data.DataLoader(trainset, batch_size=32,shuffle=True, num_workers=2)
-# testset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
-# test_loader = torch.utils.data.DataLoader(testset, batch_size=1000,shuffle=False, num_workers=2)
-test_loader,train_loader=dm.loadImages(os.getcwd()+"/Database/training-data")
+dm.transformImages()
+training =os.getcwd()+"/Database/training-data"
+test_loader,train_loader=dm.loadImages(training)
+classes = sorted(os.listdir(training))
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv_layer = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+def model_create():
 
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
+    class Net(nn.Module):
 
-        self.fc_layer = nn.Sequential(
-            nn.Dropout(p=0.1),
-            nn.Linear(in_features=32 * 32 * 24, out_features=num_classes),
-            # nn.Linear(8 * 8 * 64, 1000),
-            nn.ReLU(inplace=True),
-            nn.Linear(1000, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.1),
-            nn.Linear(512, 10)
-        )
 
-    def forward(self, x):
-        # conv layers
-        x = self.conv_layer(x)
-        # flatten
-        x = x.view(x.size(0),-1)
-        # fc layer
-        x = self.fc_layer(x)
-        return x
+        # Defining the Constructor
+        def __init__(self, num_classes=3):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3, stride=1, padding=1)
+            self.pool = nn.MaxPool2d(kernel_size=2)
+            self.drop = nn.Dropout2d(p=0.2)
+            self.fc = nn.Linear(in_features=32 * 32 * 24, out_features=num_classes)
 
-model = CNN()
-criterion = nn.CrossEntropyLoss()
+        def forward(self, x):
+            x = F.relu(self.pool(self.conv1(x)))
+            x = F.relu(self.pool(self.conv2(x)))
+            x = F.dropout(self.drop(x), training=self.training)
+            x = x.view(-1, 32 * 32 * 24)
+            x = self.fc(x)
+            return torch.log_softmax(x, dim=1)
+
+    # Create an instance of the model class and allocate it to the device
+    model = Net(num_classes=len(classes))
+    print(model)
+    return model
+
+def model_train(model, train_loader, optimizer, num_epochs):
+    # Set the model to training mode
+    model.train()
+    total_step = len(train_loader)
+    loss_list = []
+    acc_list = []
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            # Forward pass
+            outputs = model(images)
+            loss = loss_criteria(outputs, labels)
+            loss_list.append(loss.item())
+            # Backprop and optimisation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            # Train accuracy
+            total = labels.size(0)
+            _,predicted = torch.max(outputs.data, 1)
+            correct = (predicted == labels).sum().item()
+            acc_list.append(correct / total)
+
+
+        print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'.
+              format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
+                     (correct / total) * 100))
+
+def test_model(model, test_loader):
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        print('Test Accuracy of the model on the 10000 test images: {} %'
+              .format((correct / total) * 100))
+
+model=model_create()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-
-# Model training
-total_step = len(train_loader)
-loss_list = []
-acc_list = []
+loss_criteria = nn.CrossEntropyLoss()
+print('Training Model')
+model_train(model, train_loader, optimizer, num_epochs)
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss_list.append(loss.item())
-        # Backprop and optimisation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        # Train accuracy
-        total = labels.size(0)
-        _,predicted = torch.max(outputs.data, 1)
-        correct = (predicted == labels).sum().item()
-        acc_list.append(correct / total)
-
-    if (i + 1) % 100 == 0:
-        print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-              .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
-                      (correct / total) * 100))
+    test_model(model, test_loader)
 
 
-#      Model Evaluation
-
+actual = []
+predictions = []
 model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        outputs = model(images)
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum().item()
-    print('Test Accuracy of the model on the 10000 test images: {} %'
-          .format((correct / total) * 100))
+for data, target in test_loader:
+    for label in target.data.numpy():
+        actual.append(label)
+    for prediction in model(data).data.numpy().argmax(1):
+        predictions.append(prediction)
+
+cm = np.array(confusion_matrix(actual, predictions))
+cr =classification_report(actual,predictions)
+print("Confusion Matrix")
+print(cm)
+print("Classification Report")
+print(cr)
+
+def plot_ConfusionMatrix(confusion_matrix):
+    figure = plt.figure()
+    axes = figure.add_subplot(111)
+    axes.matshow(cm, interpolation ='nearest')
+    for (i, j), z in np.ndenumerate(cm):
+        axes.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
+    axes.set_xticklabels(['']+classes)
+    axes.set_yticklabels(['']+classes)
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.show()
+plot_ConfusionMatrix(cm)
+
