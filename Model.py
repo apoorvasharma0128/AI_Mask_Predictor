aacf492ffd5aa572c14ddb6b0e6d15bd0d30c901
apoorvasharma0128 <apoorvasharma0128@gmail.com>
2020-11-21 00:38:20 -0500

    Update Model.py

--- Model.py
+++ Model.py
@@ -8,11 +8,14 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import numpy as np
 
-num_epochs = 4
+num_epochs = 10
 num_classes = 3
 learning_rate = 0.001
-dm.transformImages()
 training =os.getcwd()+"/Database/training-data"
+
+transformMsg=dm.transformImages()
+print("Transformation Complete. Loading Data")
+print(transformMsg)
 test_loader,train_loader=dm.loadImages(training)
 classes = sorted(os.listdir(training))
 
@@ -20,26 +23,24 @@ def model_create():
 
     class Net(nn.Module):
 
-
-        # Defining the Constructor
         def __init__(self, num_classes=3):
             super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
-            self.conv2 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3, stride=1, padding=1)
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+            self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
             self.pool = nn.MaxPool2d(kernel_size=2)
-            self.drop = nn.Dropout2d(p=0.2)
-            self.fc = nn.Linear(in_features=32 * 32 * 24, out_features=num_classes)
+            self.drop = nn.Dropout2d(p=0.1)
+            self.fc = nn.Linear(in_features=64 * 64* 32, out_features=num_classes)
 
         def forward(self, x):
             x = F.relu(self.pool(self.conv1(x)))
             x = F.relu(self.pool(self.conv2(x)))
             x = F.dropout(self.drop(x), training=self.training)
-            x = x.view(-1, 32 * 32 * 24)
+            x = x.view(-1, 64 * 64 * 32)
             x = self.fc(x)
             return torch.log_softmax(x, dim=1)
 
-    # Create an instance of the model class and allocate it to the device
     model = Net(num_classes=len(classes))
+    print("Model Summary")
     print(model)
     return model
 
@@ -80,7 +81,7 @@ def test_model(model, test_loader):
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
-        print('Test Accuracy of the model on the 10000 test images: {} %'
+        print('Test Accuracy of the model: {} %'
               .format((correct / total) * 100))
 
 model=model_create()
@@ -88,8 +89,8 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 loss_criteria = nn.CrossEntropyLoss()
 print('Training Model')
 model_train(model, train_loader, optimizer, num_epochs)
-for epoch in range(num_epochs):
-    test_model(model, test_loader)
+print('Testing Model')
+test_model(model, test_loader)
 
 
 actual = []
@@ -108,16 +109,20 @@ print(cm)
 print("Classification Report")
 print(cr)
 
-def plot_ConfusionMatrix(confusion_matrix):
+def plot_ConfusionMatrix(confusion_matrix,label):
+
     figure = plt.figure()
-    axes = figure.add_subplot(111)
-    axes.matshow(cm, interpolation ='nearest')
-    for (i, j), z in np.ndenumerate(cm):
-        axes.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
-    axes.set_xticklabels(['']+classes)
-    axes.set_yticklabels(['']+classes)
+    ax = figure.add_subplot(111)
+
+    ax.matshow(cm, interpolation ='nearest')
+    for (x, y), z in np.ndenumerate(cm):
+        ax.text(x, y, '{:0.1f}'.format(z), ha='center', va='center')
+    ax.set_xticklabels(['']+classes)
+    ax.set_yticklabels(['']+classes)
     plt.xlabel("Predicted")
     plt.ylabel("Actual")
     plt.show()
-plot_ConfusionMatrix(cm)
+plot_ConfusionMatrix(cm,"Confusion Matrix")
 
+def plotLearningCurve(accuracy,epoch):
+    print()
